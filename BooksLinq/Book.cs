1a3d61dfd7a20543ba47ed2e64e9e350a4a7a5e6
HARRIS MAHMOUD <D3158960@live.tees.ac.uk>
2024-10-03 12:07:49 +0100

    Tutorial on books page 8 of 7

---
+++ BooksLinq/Book.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooksLinq
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int PageCount { get; set; }
+        public string Publisher { get; set; }
+        public int PublishedYear { get; set; }
+        public string Genre { get; set; }
+        public bool IsClassic { get; set; }
+
+
+        // Default constructor
+        public Book()
+        {
+            Id = 0;
+            Title = "Unknown";
+            Author = "Unknown";
+            PageCount = 0;
+            Publisher = "Unknown";
+            PublishedYear = 0;
+            Genre = "Unknown";
+            IsClassic = false;
+        }
+
+        public Book(int id, string title, string author, int pageCount,
+         string publisher, int publishedYear, string genre,
+         bool isClassic)
+        {
+            Id = id;
+            Title = title;
+            Author = author;
+            PageCount = pageCount;
+            Publisher = publisher;
+            PublishedYear = publishedYear;
+            Genre = genre;
+            IsClassic = isClassic;
+        }
+
+        public override string ToString()
+        {
+            return $"{Id}: Author: {Author}\n Published: ({PublishedYear}).\n Title: {Title}.\n Publisher: {Publisher}.\n " +
+                   $"Pages: {PageCount}\n Genre: {Genre}\n Classic: {IsClassic}\n";
+        }
+
+
+
+
+
+    }
+}
--- BooksLinq/Program.cs
+++ BooksLinq/Program.cs
@@ -1,2 +1,91 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using BooksLinq;
+
+
+
+
+Console.WriteLine("Book List LINQ\n");
+
+var books = new List<Book>
+{
+    new Book(1, "The Catcher in the Rye", "J.D. Salinger", 277,
+            "Little, Brown and Company", 1951, "Fiction", true),
+    new Book(2, "War and Peace", "Leo Tolstoy", 1225,
+            "The Russian Messenger (serial)", 1869, "Historical Fiction", true),
+    new Book(3, "1984", "George Orwell", 328,
+            "Secker & Warburg", 1949, "Dystopian", true)
+};
+
+static void DisplayBookList(List<Book> bookList)
+{
+    foreach (var book in bookList)
+    {
+        Console.WriteLine(book);
+    }
+}
+
+
+
+books.AddRange(new List<Book>
+   {
+      new Book(4, "The Hobbit", "J.R.R. Tolkien", 310,
+                "George Allen & Unwin", 1937, "Fantasy", true),
+      new Book(5, "Tender Is the Night", "F. Scott Fitzgerald", 317,
+                "Charles Scribner", 1934, "Fiction", true),
+      new Book(6, "The Great Gatsby", "F. Scott Fitzgerald", 180,
+                "Charles Scribner", 1925, "Fiction", true),
+      new Book(7, "To Kill a Mockingbird", "Harper Lee", 336,
+                "J.B. Lippincott & Co.", 1960, "Fiction", true),
+      new Book(8, "The Alchemist", "Paulo Coelho", 208,
+                "HarperTorch", 1988, "Adventure", true),
+      new Book(9, "Brave New World", "Aldous Huxley", 268,
+                "Chatto & Windus", 1932, "Dystopian", true),
+      new Book(10, "Moby-Dick", "Herman Melville", 635,
+                "Harper & Brothers", 1851, "Adventure", true),
+      new Book(11, "Pride and Prejudice", "Jane Austen", 279,
+                "T. Egerton, Whitehall", 1813, "Romance", true),
+      new Book(12, "The Lord of the Rings", "J.R.R. Tolkien", 1178,
+                "George Allen & Unwin", 1954, "Fantasy", true),
+      new Book(13, "Becoming", "Michelle Obama", 448,
+                "Crown", 2018, "Autobiography", false),
+      new Book(14, "Sapiens: A Brief History of Humankind", "Yuval Harari", 443,
+                "Harper", 2011, "Non-Fiction", false),
+      new Book(15, "Educated", "Tara Westover", 334,
+                "Random House", 2018, "Autobiography", false),
+      new Book(17, "Thinking, Fast and Slow", "Daniel Kahneman", 499,
+                "Farrar, Straus and Giroux", 2011, "Non-Fiction", false),
+      new Book(18, "The Body Keeps the Score", "Bessel van der Kolk", 464,
+            "Viking", 2014, "Non-Fiction", false)
+   }
+);
+
+DisplayBookList(books);
+
+Console.WriteLine("\nTolkien Books");
+var tolkienBooks = books
+    .Where(b => b.Author == "J.R.R. Tolkien")
+    .ToList();
+
+DisplayBookList(tolkienBooks);
+
+
+
+Console.WriteLine("\nTolkien or Coelho Books");
+var tolkienOrCoelhoBooks = books
+    .Where (b => b.Author == "J.R.R Tolkien" || b.Author == "Paulo Coelho")
+    .ToList ();
+
+DisplayBookList(tolkienOrCoelhoBooks);
+
+Console.WriteLine("\nBooks with IS in the title");
+
+var isBooks = books
+    .Where(b => b.Title.Contains("is", StringComparison.CurrentCultureIgnoreCase))
+    .ToList();
+
+DisplayBookList(isBooks);
+
+
+Console.WriteLine("\nBooks with a page count between 450 & 1000 (inclusive)");
+
+
